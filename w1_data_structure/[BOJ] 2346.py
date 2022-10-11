c28e6af6c43828a8b48015818df713c297d88c4f
Jungeunkim-dev <svaha@ewhain.net>
2022-10-11 10:31:02 +0900

    [풍선 터트리기] 주석 추가

--- w1_data_structure/[BOJ] 2346.py
+++ w1_data_structure/[BOJ] 2346.py
@@ -2,6 +2,7 @@ from collections import deque
 
 N = int(input()) # 풍선의 개수 
 
+# 풍선의 index 값은 보존되어야 함 -> enumerate 함수 사용
 queue = deque(enumerate(map(int, input().split()))) # 풍선 내부 종이의 수 리스트
 result = []
 
@@ -10,8 +11,8 @@ while queue:
   result.append(index+1) # 풍선의 번호는 1부터 시작
 
   if val > 0:
-    queue.rotate(-(val-1)) # 시계 방향 회전
+    queue.rotate(-(val-1)) # -(양수) = 음수 -> rotate 왼쪽으로
   else:
-    queue.rotate(-val) # 시계 반대 방향 회전 
+    queue.rotate(-val) # -(음수 값) = 양수 -> rotate 오른쪽
 
 print(' '.join(map(str, result)))
